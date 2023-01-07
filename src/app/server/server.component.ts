6f0e9ad6ab0cb0d86448fa286a7499a4865a17b5
ArupKDas <akdcse2k10@gmail.com>
2023-01-07 11:21:12 +0600

    Data binding, Properties changes

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.css']
 })
-export class ServerComponent {
-
+export class ServerComponent implements OnInit{
+  
+  serverId: number = 10;
+  serverStatus: string = 'offline'
+  allowNewServer = false;
+  serverCreated = false;
+  serverCreationStatus = 'No server is created';
+  serverName = '';
+  userName = '';
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000);
+  }
+  ngOnInit(): void {
+  }
+  onCreateServer(){
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server is created...'+this.serverName;
+  }
+  onUpdateServerName(event: any){
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+  isUserNameEmpty(){
+    return this.userName.length == 0;
+  }
+  onResetUser(){
+    this.userName = '';
+  }
 }
